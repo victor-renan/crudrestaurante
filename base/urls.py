931eef71310d89f08a94429488057c768f7557a4
Renan Alves <alvesrenanpro@gmail.com>
2023-01-07 15:32:21 -0300

    Login e registro corrigidos, home feita

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("cardapio/",  views.sobre, name="cardapio"),
+    path("cardapio/",  views.cardapio, name="cardapio"),
     path("categorias/",  views.categorias, name="categorias"),
     path("sobre/",  views.sobre, name="sobre"),
     path("login/", views.login_view, name="login"),
